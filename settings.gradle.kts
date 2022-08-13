fdff627726857df37aee2d280e66c97084344ec0
Sergey Okatov <sokatov@gmail.com>
2022-08-13 11:22:36 +0500

    Full test is added for building derivative openApi specs and code generation

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,9 +3,17 @@ rootProject.name = "crowdproj-root"
 pluginManagement {
     plugins {
         val kotlinVersion: String by settings
+        val openapiVersion: String by settings
+
         kotlin("jvm") version kotlinVersion
-//        kotlin("multiplatform") version kotlinVersion
+        kotlin("multiplatform") version kotlinVersion
+        kotlin("plugin.serialization") version kotlinVersion apply false
+
+        id("org.openapi.generator") version openapiVersion apply false
     }
 }
+val runTests: String by settings
+val shouldTest = runTests.toBoolean()
+
 include("specs-v0")
-//include("specs-v0-test")
+if(shouldTest) include("specs-v0-test")
--- specs-v0-test/build.gradle.kts
+++ specs-v0-test/build.gradle.kts
@@ -1,8 +1,10 @@
 plugins {
-    kotlin("jvm")
+    kotlin("multiplatform")
+    id("org.openapi.generator")
+    kotlin("plugin.serialization")
 }
-val apiSpec: Configuration by configurations.creating
 
+val apiSpec: Configuration by configurations.creating
 dependencies {
     apiSpec(
         group = "com.crowdproj",
@@ -13,16 +15,102 @@ dependencies {
     )
 }
 
-tasks {
-    val getSpecs by creating {
-        doFirst {
-            copy {
-                from(apiSpec.asPath)
-                into("$buildDir/generate/")
+kotlin {
+    jvm { withJava() }
+    linuxX64 { }
+    macosX64 { }
+
+    artifacts {
+
+    }
+    sourceSets {
+        val serializationVersion: String by project
+
+        @Suppress("UNUSED_VARIABLE")
+        val commonMain by getting {
+
+            kotlin.srcDirs("$buildDir/generate-resources/main/src/commonMain/kotlin")
+            dependencies {
+                implementation(kotlin("stdlib-common"))
+
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:$serializationVersion")
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:$serializationVersion")
             }
         }
-        apiSpec.asFileTree.forEach {
-            println("SPEC: $it")
+
+        @Suppress("UNUSED_VARIABLE")
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test-common"))
+                implementation(kotlin("test-annotations-common"))
+            }
         }
+
+        @Suppress("UNUSED_VARIABLE")
+        val jvmTest by getting {
+            dependencies {
+                implementation(kotlin("test-junit"))
+            }
+        }
+    }
+}
+
+/**
+ * Настраиваем генерацию здесь
+ */
+openApiGenerate {
+    val openapiGroup = "${rootProject.group}.api.v2"
+    generatorName.set("kotlin") // Это и есть активный генератор
+    packageName.set(openapiGroup)
+    apiPackage.set("$openapiGroup.api")
+    modelPackage.set("$openapiGroup.models")
+    invokerPackage.set("$openapiGroup.invoker")
+    inputSpec.set("$projectDir/spec-crowdproj-test.yaml")
+    library.set("multiplatform")
+
+    /**
+     * Здесь указываем, что нам нужны только модели, все остальное не нужно
+     */
+    globalProperties.apply {
+        put("models", "")
+        put("modelDocs", "false")
+    }
+
+    /**
+     * Настройка дополнительных параметров из документации по генератору
+     * https://github.com/OpenAPITools/openapi-generator/blob/master/docs/generators/kotlin.md
+     */
+    configOptions.set(
+        mapOf(
+            "dateLibrary" to "string",
+            "enumPropertyNaming" to "UPPERCASE",
+            "collectionType" to "list",
+        )
+    )
+}
+
+val getSpecs by tasks.creating {
+    doFirst {
+        copy {
+            from(apiSpec.asPath)
+            into("$buildDir")
+            rename { "base.yaml" }
+        }
+    }
+}
+
+tasks {
+    this.openApiGenerate {
+        dependsOn(getSpecs)
+    }
+}
+
+afterEvaluate {
+    val openApiGenerate = tasks.getByName("openApiGenerate")
+    tasks.filter { it.name.startsWith("compile") }.forEach {
+        it.dependsOn(openApiGenerate)
+    }
+    tasks.filter { it.name.endsWith("Elements") }.forEach {
+        it.dependsOn(openApiGenerate)
     }
 }
